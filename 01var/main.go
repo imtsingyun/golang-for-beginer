74a71b44737d46479873495cb94a1888182a79a5
tsingyun <imtsingyun@gmail.com>
2022-01-12 16:32:49 +0800

    Update main.go

--- 01var/main.go
+++ 01var/main.go
@@ -14,12 +14,21 @@ var (
 	height float32
 )
 
+const pi = 3.14;
+
 func main() {
 	name = "tom"
 	age = 19
 	isOk = true
 	
-	fmt.Printf("name: %s, age: %d, isOk: %t", name, age, isOk)
-	fmt.Printf("color: %s, weight: %f, height: %f", color, weight, height)
+	fmt.Printf("name: %s, age: %d, isOk: %t\n", name, age, isOk)
+	fmt.Printf("color: %s, weight: %f, height: %f\n", color, weight, height)
 	
+	var s1 string = "abc"
+	var s2 = "efg";
+	fmt.Printf("s1=%s, s2= %s\n", s1, s2)
+	
+	s3 := "xyx";
+	fmt.Println(s3)
+	fmt.Println(pi)	
 }
