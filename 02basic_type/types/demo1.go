835096e5f411d94d620354b41ae152c2a394c128
tsingyun <imtsingyun@gmail.com>
2022-01-14 14:32:52 +0800

    test

---
+++ 02basic_type/types/demo1.go
@@ -0,0 +1,13 @@
+package types
+
+import "fmt"
+
+func Zimianzhi() {
+	n := 109
+	fmt.Printf("%b\n", n) // binary 2
+	fmt.Printf("%d\n", n) // decimal 10
+	fmt.Printf("%o\n", n) // Octal 8
+	fmt.Printf("%x\n", n) // hex 16  lower case 
+	fmt.Printf("%X\n", n) // hex 16  capital
+	fmt.Printf("%p\n", &n) // memory address 0xc0000aa058
+}
